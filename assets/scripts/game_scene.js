5de08f5c90de5589a0f6fd8ebd5ff6a9bd8a1a38
132982jianan <1072772483@qq.com>
2018-09-22 17:35:22 +0800

    。。

--- assets/scripts/game_scene.js
+++ assets/scripts/game_scene.js
@@ -1,37 +1,20 @@
-// Learn cc.Class:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
-// Learn Attribute:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+/**
+ * 游戏场景
+ * 普通球 + 白球 + 球杆
+ */
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+
+        // 所有普通球
         ball_root: {
             type: cc.Node,
             default: null,
         },
 
+        //  白球 
         white_ball: {
             type: cc.Node,
             default: null,
@@ -52,7 +35,10 @@ cc.Class({
             b.getComponent("ball").reset();
         }
 
+        // 还可以这样调用组件实例的方法
         this.white_ball.getComponent("white_ball").reset();
+
+        // 
         this.is_game_started = true;
     },
 
